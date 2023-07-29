c052b783dd9ead097b52dbea59426401ae512212
JhalaKanishk <140880609+JhalaKanishk@users.noreply.github.com>
2023-07-29 13:41:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function run(){
+    // variables
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+    let output= document.getElementById("output");
+
+
+    // Display the values in Output
+    // for html
+    output.contentDocument.body.innerHTML = htmlCode+"<style>"+cssCode+"</style>";
+    output.contentWindow.eval(jsCode);
+}
